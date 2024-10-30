76b20ef6a3cda6e9cc09f250e780aeb09a2a72f4
izbatSL2 <izbat@sustainablelivinglab.org>
2024-10-30 21:03:43 +0700

    adding ai_service for generate question survey

--- ai/ai_service.py
+++ ai/ai_service.py
@@ -1,15 +1,24 @@
-from openai import OpenAI
-model = "gpt-4o-2024-08-06"
+from typing import Dict, Any
+from ai.generate import analysisTQ,multipleChoices,checkBoxes,likertS, openED
+import asyncio
+import json
 
 
+async def generate_survey_questions(survey_data: Dict[Any, Any]) -> Dict[str, str]:
+    analyse = await analysisTQ(survey_data)
+    multipleChocies = await multipleChoices(data=survey_data, analysed_data=analyse)
+    checkbox = await checkBoxes(data=survey_data, analysed_data=analyse)
+    likert_scale = await likertS(data=survey_data, analysed_data=analyse)
+    open_ended = await openED(data=survey_data, analysed_data=analyse)
+    
+    # Menggabungkan semua hasil menjadi satu daftar `survey_questions`
+    survey_questions = []
 
-api_key = OPENAI_KEY
-
-# Inisialisasi client dengan API key
-client = OpenAI(
-    api_key=api_key,
-)
-
-
-
-
+    # Tambahkan setiap hasil dari fungsi ke dalam survey_questions
+    survey_questions.append(multipleChocies)  # multiple_choice questions
+    survey_questions.append(checkbox)          # checkbox questions
+    survey_questions.append(likert_scale)      # likert scale questions
+    survey_questions.append(open_ended)        # o
+    
+    return {"survey_questions": survey_questions}
+    
