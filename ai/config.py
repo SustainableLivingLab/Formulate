cb0beb9ebe0b7598e9731aed00c535e6f5c82535
izbatSL2 <izbat@sustainablelivinglab.org>
2024-11-01 07:53:24 +0700

    updating config

--- ai/config.py
+++ ai/config.py
@@ -2,7 +2,7 @@ from openai import AsyncOpenAI
 
 
 
-api_key = "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"
+api_key = "OPENAI_API_KEY"
 
 # Inisialisasi client dengan API key
 client = AsyncOpenAI(
